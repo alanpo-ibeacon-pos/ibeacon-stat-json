fceae2c31c69f45f39cb007720b4edc2fedc7709
hkgsherlock <hkgsherlock@gmail.com>
2015-05-10 19:32:55 +0800

    wk ver @ 20150510 1932

--- get_stat.php
+++ get_stat.php
@@ -25,56 +25,79 @@ try {
     $db = new SQLite3($dir . '/ibeacons.sqlite3');
     unset($dir);
 
-    $s_q_mac_r = $db->prepare("SELECT uuid, major, minor FROM traces WHERE datetime BETWEEN :dateStart AND :dateEnd GROUP BY uuid, major, minor");
+    $s_q_mac_r = $db->prepare("SELECT hex(uuid) AS uuid, major, minor FROM traces WHERE datetime BETWEEN :dateStart AND :dateEnd GROUP BY uuid, major, minor");
     $s_q_mac_r->bindValue(':dateStart', $from->format('Y-m-d H:i:s'), SQLITE3_TEXT);
     $s_q_mac_r->bindValue(':dateEnd', $to->format('Y-m-d H:i:s'), SQLITE3_TEXT);
     $q_mac_r = $s_q_mac_r->execute();
-//     $q_mac_r = $db->query('SELECT uuid, major, minor
-//                      FROM traces
-//                      WHERE '.'
-//                    datetime BETWEEN \'' . $from->format('Y-m-d H:i:s') . '\' AND \'' . $to->format('Y-m-d H:i:s') . '\'
-//                      GROUP BY uuid, major, minor');
-    // $q_mac_r->execute(array(':selfMac' => hexdec($selfMac))); # performace: slow
 
     $arr_stat = array();
 
-    $s_q_ib = $db->prepare('SELECT AVG(txpower) AS txpower, AVG(rssi) AS rssi, MIN(datetime) AS firstSeen, MAX(datetime) AS lastSeen FROM (
-                                SELECT _idx, txpower, rssi, datetime
-                                FROM (
-                                    SELECT @rownum := @rownum + 1 AS _idx, txpower, rssi, datetime
-                                    FROM traces, (SELECT @rownum := 0) r
-                                    WHERE uuid = :uuid && major = :major && minor = :minor AND datetime BETWEEN :dateStart AND :dateEnd
-                                ) limited_traces
-                                WHERE _idx >= ROUND(FOUND_ROWS() * 0.1)
-                                && _idx < ROUND(FOUND_ROWS() * 0.9)
-                            ) AS stat_traces');
     while ($row_mac_r = $q_mac_r->fetchArray(SQLITE3_ASSOC)) {
-        $s_q_ib->bindValue(':uuid', $row_mac_r['uuid'], SQLITE3_BLOB);
-        $s_q_ib->bindValue(':major', $row_mac_r['major'], SQLITE3_INTEGER);
-        $s_q_ib->bindValue(':minor', $row_mac_r['minor'], SQLITE3_INTEGER);
-        $s_q_mac_r->bindValue(':dateStart', $from->format('Y-m-d H:i:s'), SQLITE3_TEXT);
-        $s_q_mac_r->bindValue(':dateEnd', $to->format('Y-m-d H:i:s'), SQLITE3_TEXT);
+        $uuid = $row_mac_r['uuid'];
+        $major = $row_mac_r['major'];
+        $minor = $row_mac_r['minor'];
 
-        $q_ib = $s_q_ib->execute();
+//        $s_q_ib = $db->prepare('SELECT txpower, rssi, datetime
+//                                FROM traces
+//                                WHERE uuid = :uuid
+//                                AND major = :major
+//                                AND minor = :minor
+//                                AND datetime BETWEEN :dateStart AND :dateEnds
+//                                ORDER BY datetime ASC');
+//        $s_q_ib->bindValue(':uuid', pack('H*', $uuid), SQLITE3_TEXT);
+//        $s_q_ib->bindValue(':major', $major, SQLITE3_INTEGER);
+//        $s_q_ib->bindValue(':minor', $minor, SQLITE3_INTEGER);
+//        $s_q_ib->bindValue(':dateStart', $from->format('Y-m-d H:i:s'), SQLITE3_TEXT);
+//        $s_q_ib->bindValue(':dateEnd', $to->format('Y-m-d H:i:s'), SQLITE3_TEXT);
+//
+//        $q_ib = $s_q_ib->execute();
 
-        if (!$q_ib->numColumns() || $q_ib->columnType(0) == SQLITE3_NULL) {
-            http_response_code(500);
-            header('Content-Type: text/plain');
-            echo 'empty result when getting avg txpower and rssi for uuid: ' . bin2hex($row_mac_r['uuid']) . ', major: ' . $row_mac_r['major'] . ', minor: ' . $row_mac_r['minor'];
-            die(1);
+        $sql = 'SELECT txpower, rssi, datetime
+                                FROM traces
+                                WHERE uuid = x\'' . $uuid . '\'
+                                AND major =  ' . $major . '
+                                AND minor = ' . $minor . '
+                                AND datetime BETWEEN \'' . $from->format('Y-m-d H:i:s') . '\' AND \'' . $to->format('Y-m-d H:i:s') . '\'
+                                ORDER BY datetime ASC';
+        $q_ib = $db->query($sql);
+        unset($sql);
+
+        if (!$q_ib->numColumns() /*|| $q_ib->columnType(0) == SQLITE3_NULL*/) {
+            throw new Exception('empty result when getting avg txpower and rssi for uuid: ' . bin2hex($row_mac_r['uuid']) . ', major: ' . $row_mac_r['major'] . ', minor: ' . $row_mac_r['minor']);
         }
 
-        $row_ib = $q_ib->fetchArray(SQLITE3_ASSOC);
+        $firstSeen = null;
+        $lastSeen = null;
+        $arr_bcn_data = array();
+        while ($row_ib = $q_ib->fetchArray(SQLITE3_ASSOC)) {
 
-        // actually a overhead-excluded version of arr_push
+            if ($firstSeen == null) $firstSeen = $row_ib['datetime'];
+            $lastSeen = $row_ib['datetime'];
+            $arr_bcn_data[] = $row_ib;
+        }
+
+        $sumTxPower = 0.0;
+        $sumRssi = 0.0;
+        $sampleCount = 0;
+        $first10 = (int) ( count($arr_bcn_data) * 0.1 );
+        $last10 = (int) ( count($arr_bcn_data) * 0.9 );
+        for ($sampleCount = $first10; $sampleCount < $last10; $sampleCount++) {
+            $e = $arr_bcn_data[$sampleCount];
+
+            $sumTxPower += $e['txpower'];
+            $sumRssi += $e['rssi'];
+        }
+        $sampleCount -= $first10;
+
+        // actually an overhead-excluded version of arr_push
         $arr_stat[] = [
-            'uuid' => bin2hex($row_mac_r['uuid']),
-            'major' => $row_mac_r['major'],
-            'minor' => $row_mac_r['minor'],
-            'rssi' => floatval($row_ib['rssi']),
-            'txpower' => floatval($row_ib['txpower']),
-            'firstSeen' => $row_ib['firstSeen'],
-            'lastSeen' => $row_ib['lastSeen']
+            'uuid' => strtolower($uuid),
+            'major' => $major,
+            'minor' => $minor,
+            'rssi' => ($sumRssi / $sampleCount),
+            'txpower' => ($sumTxPower / $sampleCount),
+            'firstSeen' => $firstSeen,
+            'lastSeen' => $lastSeen
         ];
     }
 
@@ -83,6 +106,7 @@ try {
 
 } catch (Exception $e) {
     http_response_code(500);
-    echo $e;
+    header('Content-Type: text/plain');
+    echo $e->getMessage();
     die(1);
 }
